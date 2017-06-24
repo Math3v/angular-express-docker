69bf4cc3279443be00be4b7db89a5e666f166c5b
Matej Minarik <mato.minarik@azet.sk>
2017-06-24 19:36:33 +0200

    List users from MongoDB

--- express-server/routes/api.js
+++ express-server/routes/api.js
@@ -1,8 +1,33 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
+const dbHost = 'mongodb://database/mean-docker';
+mongoose.connect(dbHost);
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  age: Number
+});
+
+const User = mongoose.model('User', userSchema);
+
+const user = new User();
+user.name = 'Matej';
+user.age = 25;
+user.save((err) => {
+  console.error(`Cannot create user: ${err}`);
+});
+
 router.get('/', (req, res) => {
   res.send('API Works!');
+});
+
+router.get('/users', (req, res) => {
+  User.find({}, (err, users) => {
+    if(err) res.status(500).send(err);
+    res.status(200).json(users);
+  });
 });
 
 module.exports = router;
--- ng-client/src/app/app.component.ts
+++ ng-client/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'app works!';
+
+  users$: Observable<any>;
+
+  constructor(
+    private http: Http
+  ) {}
+
+  ngOnInit() {
+    this.users$ = this.http.get('http://localhost:3000/users').map(res => res.json());
+  }
 }
